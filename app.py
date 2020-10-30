97bb69b783f05ca487d83ba83e34c21dc1c24b2f
Raymond Wiker <rayw@equinor.com>
2020-10-30 10:45:48 +0100

    Added OAUTH2 authentication.

--- app.py
+++ app.py
@@ -1,15 +1,100 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
+from flask_session import Session
+import msal
+import uuid
+import app_config
+
 
 app = Flask("name-gathering",
     static_url_path='', 
     static_folder='frontend/build',
     template_folder='frontend/build')
 
+app.config.from_object(app_config)
+
+Session(app)
+
 
 @app.route("/")
 def root():
+    if not session.get("user"):
+        return redirect(url_for("login"))
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    session["state"] = str(uuid.uuid4())
+    auth_url = _build_msal_app().get_authorization_request_url(
+        app_config.SCOPE,  # Technically we can use empty list [] to just sign in,
+                           # here we choose to also collect end user consent upfront
+        state=session["state"],
+        redirect_uri=url_for("authorized", _external=True))
+    return redirect(auth_url)
+
+@app.route("/authorized")
+def authorized():
+    if request.args['state'] != session.get("state"):
+        return redirect(url_for("login"))
+    cache = _load_cache()
+    result = _build_msal_app(cache).acquire_token_by_authorization_code(
+        request.args['code'],
+        scopes=app_config.SCOPE,  # Misspelled scope would cause an HTTP 400 error here
+        redirect_uri=url_for("authorized", _external=True))
+    if "error" in result:
+        return "Login failure: %s, %s" % (
+            result["error"], result.get("error_description"))
+    session["user"] = result.get("id_token_claims")
+    _save_cache(cache)
+    return redirect(url_for("root"))    
+
+@app.route("/logout")
+def logout():
+    session["user"] = None  # Log out from this app from its session
+    # session.clear()  # If you prefer, this would nuke the user's token cache too
+    return redirect(  # Also need to logout from Microsoft Identity platform
+        "https://login.microsoftonline.com/common/oauth2/v2.0/logout"
+        "?post_logout_redirect_uri=" + url_for("root", _external=True))
+
+@app.route("/graphcall")
+def graphcall():
+    token = _get_token_from_cache(app_config.SCOPE)
+    if not token:
+        return redirect(url_for("login"))
+    graph_data = requests.get(  # Use token to call downstream service
+        app_config.ENDPOINT,
+        headers={'Authorization': 'Bearer ' + token['access_token']},
+        ).json()
+    return render_template('display.html', result=graph_data)
+
+@app.route("/userinfo")
+def userinfo():
+    return session["user"]
+
+
+def _load_cache():
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
+    return cache
+
+def _save_cache(cache):
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
+
+def _build_msal_app(cache=None):
+    return msal.ConfidentialClientApplication(
+        app_config.CLIENT_ID, authority=app_config.AUTHORITY,
+        client_credential=app_config.CLIENT_SECRET, token_cache=cache)
+
+def _get_token_from_cache(scope=None):
+    cache = _load_cache()  # This web app maintains one cache per session
+    cca = _build_msal_app(cache)
+    accounts = cca.get_accounts()
+    if accounts:  # So all account(s) belong to the current signed-in user
+        result = cca.acquire_token_silent(scope, account=accounts[0])
+        _save_cache(cache)
+        return result
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
---
+++ app_config.py
@@ -0,0 +1,19 @@
+import os
+
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+if not CLIENT_SECRET:
+    raise ValueError("Need to define CLIENT_SECRET environment variable")
+
+AUTHORITY = "https://login.microsoftonline.com/3aa4a235-b6e2-48d5-9195-7fcf05b459b0"
+
+CLIENT_ID = "f932b0a6-1a23-489a-b309-274f455a7758"
+
+# You can find more Microsoft Graph API endpoints from Graph Explorer
+# https://developer.microsoft.com/en-us/graph/graph-explorer
+ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no admin consent
+
+# You can find the proper permission names from this document
+# https://docs.microsoft.com/en-us/graph/permissions-reference
+SCOPE = ["User.ReadBasic.All"]
+
+SESSION_TYPE = "filesystem"  # So token cache will be stored in server-side session
